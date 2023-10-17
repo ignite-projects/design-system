df23e523802e3c45377fe39e77daf7661353a1df
Robson Natanael <natanaelrobson@gmail.com>
2023-10-16 23:16:43 -0300

    refactor: category form created

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -2,7 +2,7 @@ import type { StoryObj, Meta } from '@storybook/react'
 import { Button, ButtonProps } from "@ign-ui/react";
 
 export default {
-  title: "Button",
+  title: "Form/Button",
   component: Button,
   args: {
     children: "Enviar",
