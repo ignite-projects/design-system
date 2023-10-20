14b78ba7f86e2f12699d52a89a40ec61e90046bd
Robson Natanael <natanaelrobson@gmail.com>
2023-10-19 21:03:03 -0300

    feat(docs): addon a11y added

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -10,12 +10,16 @@ function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, "package.json")));
 }
 const config: StorybookConfig = {
-  stories: ["../src/pages/**/*.mdx", "../src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: [
+    "../src/pages/**/*.mdx",
+    "../src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
   addons: [
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
     getAbsolutePath("@storybook/addon-onboarding"),
     getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath("@storybook/addon-a11y"),
   ],
   framework: {
     name: getAbsolutePath("@storybook/react-vite"),
